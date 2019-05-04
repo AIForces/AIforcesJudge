595f281bc62306fe8b6eb66b93f09d0939090e95
Nikolay <nikolayr2001@gmail.com>
2019-05-04 13:52:29 +0300

    fictitious answer

--- worker.py
+++ worker.py
@@ -4,4 +4,12 @@ import requests as rq
 
 def run(queue: mp.Queue):
     data = queue.get()
-    rq.post("http://localhost:3001/receive_data", json=data)
+    print(data)
+    resp = {
+        "query_id": 6,
+        "player_1_verdict": "OK",
+        "player_2_verdict": "ML",
+        "winner": "player_1",
+        "log": "smth"
+    }
+    rq.post("http://localhost:3001/receive_data", json=resp)

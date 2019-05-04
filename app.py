ba42a4ace0fd6219d2acfe06c232fad2e77a5a4b
Nikolay <nikolayr2001@gmail.com>
2019-05-04 12:47:35 +0300

    add correct testable answer for /judge startup doesn't done yet

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
+import os
+
 from flask import Flask
 from flask import request, jsonify
 
+
 app = Flask('AIforcesJudge')
+
+
+@app.before_first_request
+def startup():
+
+    if os.path.exists('startup.lock'):
+        return
+
+    if not os.path.exists('tmp'):
+        os.mkdir('tmp')
+
+    for folder in os.listdir('tmp'):
+        os.removedirs(folder)
+
+    app.logger.info("startup done")
 
 
 @app.route('/judge', methods=['POST'])
 def judge():
     data = request.get_json(force=True)
     print(data)
-    return jsonify("ok")
+    return jsonify({
+        "query_id": data["query_id"],
+        "player_1_verdict": "OK",
+        "player_2_verdict": "ML",
+        "winner": "player_1",
+        "log": [
+            [0, 0, 0],
+            [0, 0, 1],
+            [0, 1, 1],
+            [1, 1, 1]
+        ]
+    })
 
 
 
---
+++ judge.py
@@ -0,0 +1,7 @@
+class BaseJudge:
+
+    def __init__(self, lang1, source1, lang2, source2):
+        self.lang1 = lang1
+        self.source1 = source1
+        self.lang2 = lang2
+        self.source2 = source2

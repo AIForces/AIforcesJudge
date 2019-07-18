44e5fe0ed95af8b4c2c7dd2bec2d01bc9c13a2a8
Alekseev Aleksandr <alekseev2002@inbox.ru>
2019-07-18 20:56:20 +0300

    Update judge.py

--- judge.py
+++ judge.py
@@ -113,7 +113,7 @@ class Judge:
         elif 'python' in lang:
             source_file = f'{file_name}/{file_name}.py'
             open(source_file, 'w').write(source)
-            command = [join(file_name, join(config.PYTHON_VENV_PATH, 'python3')), f'{file_name}.py']
+            command = [join(file_name, 'python3'), f'{file_name}.py']
             print('command is:')
             print(command)
 
